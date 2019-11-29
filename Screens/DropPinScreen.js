5c932128eebe0e6507cfb70ce8715c568f92ef73
Navid <navid_hero_1st@yahoo.com>
2019-11-29 16:48:41 +0330

    finish ui

--- Screens/DropPinScreen.js
+++ Screens/DropPinScreen.js
@@ -14,20 +14,25 @@ import {
 } from "react-native";
 import { RNCamera } from 'react-native-camera';
 import Video from 'react-native-video';
+import Geolocation from 'react-native-geolocation-service';
 import ProgressBar from 'react-native-progress/Bar';
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+const audioRecorderPlayer = new AudioRecorderPlayer();
+const accessToken = "pk.eyJ1Ijoibmhlcm8iLCJhIjoiY2syZnMya2l1MGFrejNkbGhlczI1cjlnMCJ9.9QUBMhEvbP2RSkNfsjoQeA";
 
 export default class DropPinScreen extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            title: "",
             date: "Date",
             time: "Time",
             location: "Location",
             flashMode: RNCamera.Constants.FlashMode.off,
             video:true,
             audio:false,
-            type: RNCamera.Constants.Type.front,
+            type: RNCamera.Constants.Type.back,
             imageToShow: "",
             recording: "start", // [start , recording , end]
             videoTime: 0,
@@ -43,8 +48,13 @@ export default class DropPinScreen extends React.Component {
                 {id: 2, title: "Was a Foul", time: "10M"},
                 {id: 3, title: "New Zealand", time: "15M"},
                 {id: 4, title: "Azurepin", time: "59M"}
-            ]
+            ],
+            audioToPlay: null,
+            recordSecs: "",
+            recordTime: ""
         };
+
+        this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     }
     componentDidMount() {
         BackHandler.addEventListener(
@@ -62,6 +72,21 @@ export default class DropPinScreen extends React.Component {
 
     handleBackButtonPressAndroid = () => {
         return this.props.navigation.isFocused();
+    };
+
+    changeTitle(text) {
+        this.setState({title: text});
+    }
+
+    titleStyle = function() {
+        if (!(this.state.title.length > 0) && this.state.recording === "end") {
+            return {
+                borderWidth: 1,
+                borderColor: '#f00',
+            }
+        } else {
+            return {};
+        }
     };
 
     flipCamera = () => {
@@ -128,65 +153,121 @@ export default class DropPinScreen extends React.Component {
         this.setState({imageToShow: "data:image/png;base64,"+photo});
     }
 
+
     animate() {
         let startInt = 0, endInt = 60, videoTime = 0, start = "", end = "";
         let data = this;
         let refreshIntervalId = setInterval(function () {
             if (endInt > 10)
-                end = "0:" + --endInt;
+                end = "0:" + (--endInt);
             else if (endInt > 0)
-                end = "0:0" + --endInt;
-            else
+                end = "0:0" + (--endInt);
+            else {
                 clearInterval(refreshIntervalId);
+            }
 
             if (startInt === 59)
                 start = "1:00";
             else if (startInt > 8)
-                start = "0:" + ++startInt;
+                start = "0:" + (++startInt);
             else
-                start = "0:0" + ++startInt;
+                start = "0:0" + (++startInt);
 
             videoTime += 0.016;
             if (videoTime < 1)
                 data.setState({ videoTime, refreshIntervalId, start, end });
 
         }, 1000);
-        // let videoTime = 0;
-        // let start = 0;
-        // let end = 60;
-        // this.setState({ videoTime }, function() {
-        //     var refreshIntervalId = setInterval(() => {
-        //         videoTime += 0.016;
-        //         start +=1;
-        //         end -= 1;
-        //         if (videoTime > 1) {
-        //             videoTime = 1;
-        //         }
-        //         this.setState({ videoTime, refreshIntervalId, start, end });
-        //     }, 1000);
-        // });
+    }
+
+    setDateTimeLocation() {
+        // let location = "Location";
+        Geolocation.getCurrentPosition(
+            (position) => {
+                let lng = position.coords.longitude;
+                let lat = position.coords.latitude;
+                fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/' + lng + ',' + lat + '.json?access_token=' + accessToken)
+                    .then((response) => response.json())
+                    .then((responseJson) => {
+                        this.setState({location: responseJson.features[1].place_name});
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            },
+            (error) => {
+                // console.log(error.code, error.message);
+                if (error.code === 5) // Location settings are not satisfied.
+                    Alert.alert("Permission Denied", "In order to have a better experience, Azurepin needs to access your location.");
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+        // console.log(location);
+
+        let now = new Date;
+        let date = this.months[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+        let time = now.getHours() + ":" + now.getMinutes();
+        this.setState({date, time});
     }
 
     recordVideo = async () => {
-        if (this.state.recording === "start") { // start recording
-            this.animate();
-            this.setState({recording: "recording"});
-            const options = {
-                quality: 2,
-                maxDuration: 59,
-            };
-            const data = await this.camera.recordAsync(options);
-            this.onRecordVideo(data.uri);
-        } else if (this.state.recording === "recording") { // stop recording
-            this.setState({recording: "end"});
-            const data = await this.camera.stopRecording();
-        } else {
-            Alert.alert(
-                '',
-                'Your pin dropped successfully',
-                [{text: 'OK', onPress: () => this.props.navigation.pop()},],
-                {cancelable: false},
-            );
+        if (this.state.video) {
+            if (this.state.recording === "start") { // start recording
+                this.setDateTimeLocation();
+                this.animate();
+                this.setState({recording: "recording"});
+                const options = {
+                    quality: 2,
+                    maxDuration: 59,
+                };
+                const data = await this.camera.recordAsync(options);
+                this.onRecordVideo(data.uri);
+            } else if (this.state.recording === "recording") { // stop recording
+                this.setState({recording: "end"});
+                const data = await this.camera.stopRecording();
+            } else {
+                if (this.state.title.length > 0) {
+                    Alert.alert(
+                        '',
+                        'Your pin dropped successfully',
+                        [{text: 'OK', onPress: () => this.props.navigation.pop()},],
+                        {cancelable: false},
+                    );
+                } else {
+
+                }
+            }
+        } else if (this.state.audio) {
+            if (this.state.recording === "start") { // start recording
+                this.setDateTimeLocation();
+                this.animate();
+                const uri = await audioRecorderPlayer.startRecorder();
+                console.log("audio", uri);
+                audioRecorderPlayer.addRecordBackListener((e) => {
+                    this.setState({
+                        recording: "recording",
+                        recordSecs: e.current_position,
+                        recordTime: audioRecorderPlayer.mmssss(Math.floor(e.current_position)),
+                    });
+                });
+            } else if (this.state.recording === "recording") { // stop recording
+                const result = await audioRecorderPlayer.stopRecorder();
+                clearInterval(this.state.refreshIntervalId);
+                audioRecorderPlayer.removeRecordBackListener();
+                this.setState({recording: "end", audioToPlay: result, recordSecs: 0});
+                console.log("audio result", result);
+            } else {
+                if (this.state.title.length > 0) {
+                    Alert.alert(
+                        '',
+                        'Your pin dropped successfully',
+                        [{text: 'OK', onPress: () => this.props.navigation.pop()},],
+                        {cancelable: false},
+                    );
+                } else {
+
+                }
+            }
         }
     };
 
@@ -196,8 +277,35 @@ export default class DropPinScreen extends React.Component {
         this.setState({videoToShow: video});
     };
 
-    togglePlay = () => {
-        this.setState({playBack : !this.state.playBack});
+    togglePlay = async () => {
+        if (this.state.video) {
+            this.setState({playBack: !this.state.playBack});
+        } else if (this.state.audio) {
+            if (this.state.playBack === true) {
+                console.log('onStartPlay');
+                const msg = await audioRecorderPlayer.startPlayer();
+                console.log(msg);
+                audioRecorderPlayer.addPlayBackListener((e) => {
+                    if (e.current_position === e.duration) {
+                        console.log('finished');
+                        const stop = audioRecorderPlayer.stopPlayer();
+                        console.log("stop", stop);
+                        this.setState({playBack: !this.state.playBack});
+                        audioRecorderPlayer.removePlayBackListener();
+                    }
+                    this.setState({
+                        currentPositionSec: e.current_position,
+                        currentDurationSec: e.duration,
+                        playTime: audioRecorderPlayer.mmssss(Math.floor(e.current_position)),
+                        duration: audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+                    });
+                });
+            } else {
+                const msg = await audioRecorderPlayer.pausePlayer();
+                console.log("msg", msg);
+            }
+            this.setState({playBack: !this.state.playBack});
+        }
     };
 
     setModalVisible(visible) {
@@ -205,7 +313,7 @@ export default class DropPinScreen extends React.Component {
     }
 
     onExit = () => {
-        if (this.state.videoToShow.length > 0) {
+        if (this.state.videoToShow.length > 0 || this.state.audioToPlay) {
             Alert.alert(
                 'Discard Recording?',
                 'If you go back now, you will \n' +
@@ -256,6 +364,11 @@ export default class DropPinScreen extends React.Component {
                                     );
                                 })}
                             </View>
+                            <View style={{justifyContent: 'flex-end', alignItems: 'flex-end', paddingRight: 15, paddingBottom: 5}}>
+                                <TouchableOpacity onPress={() => {this.setState({modalVisible:false})}}>
+                                    <Text style={{color: "#666"}}>Close</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
                 </Modal>
@@ -275,7 +388,7 @@ export default class DropPinScreen extends React.Component {
                 </View>
                 <View style={{margin: 10, marginTop: 5, padding: 5}}>
                     <View>
-                        <TextInput placeholder="Title" style={{backgroundColor: '#DCDCDC', color: '#707070', padding: 8, borderRadius: 5, margin: 5, marginTop: 0}} />
+                        <TextInput placeholder="Title" value={this.state.title} onChangeText={(text) => {this.changeTitle(text)}} style={[styles.titleTextInput, this.titleStyle()]} />
                         <Text style={{margin: 5, color: '#666666', fontSize: 11}}>
                             <Text>{this.state.date}</Text>
                             <Text> / </Text>
@@ -289,8 +402,18 @@ export default class DropPinScreen extends React.Component {
 
                     <View style={{margin: 5, height: 330}}>
                         {this.state.audio ?
-                             <Image source={require('../assets/images/Audio.png')}
-                                    style={{marginTop: 5}}/>
+                            <View>
+                                <Image source={require('../assets/images/Audio.png')}/>
+                                {this.state.audioToPlay ?
+                                <TouchableOpacity style={styles.playContent}
+                                                  onPress={() => {
+                                                      this.togglePlay()
+                                                  }}>
+                                    <Image
+                                        source={this.state.playBack ? require('../assets/images/Play-Button.png') : require('../assets/images/Pasue-Button.png')}
+                                        style={{height: 60, width: 60}}/>
+                                </TouchableOpacity> : <Text></Text>}
+                            </View>
                             :
                             this.state.videoToShow.length > 0 ?
                                 <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'black'}}>
@@ -310,7 +433,7 @@ export default class DropPinScreen extends React.Component {
                                                       }}>
                                         <Image
                                             source={this.state.playBack ? require('../assets/images/Play-Button.png') : require('../assets/images/Pasue-Button.png')}
-                                            style={{height: 62, width: 62}}/>
+                                            style={{height: 60, width: 60}}/>
                                     </TouchableOpacity>
                                 </View>
                                 :
@@ -337,7 +460,7 @@ export default class DropPinScreen extends React.Component {
                         }
                     </View>
 
-                    <View style={{flexDirection:'row', borderRadius: 10, backgroundColor: '#DCDCDC', marginTop: 5, justifyContent: 'space-between', padding: 10}}>
+                    <View pointerEvents={this.state.recording !== "start" ? "none" : "auto"} style={[styles.actionsContainer, {opacity: this.state.recording === "start" ? 1 : 0.7}]}>
                         <TouchableOpacity disabled={!this.state.video} onPress={() => {this.toggleTorch()}}>
                             <Image source={this.state.video ? require('../assets/images/Path_11.png') : require('../assets/images/Path_12.png')}
                                    style={{width: 11, height: 23}}/>
@@ -373,6 +496,14 @@ export default class DropPinScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    titleTextInput: {
+        backgroundColor: '#DCDCDC',
+        color: '#707070',
+        padding: 8,
+        borderRadius: 5,
+        margin: 5,
+        marginTop: 0
+    },
     active: {
         color: '#4960EB'
     },
@@ -399,11 +530,11 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     playContent: {
-        height: 62,
-        width: 62,
+        height: 60,
+        width: 60,
         position: 'absolute',
-        top: '40%',
-        left: '40%'
+        top: '41%',
+        left: '42%'
     },
     progressBar: {
         flexDirection: 'row',
@@ -420,13 +551,12 @@ const styles = StyleSheet.create({
         bottom: 0
     },
     modalTitle: {
-        color: '#666666',
+        color: '#666',
         fontSize: 16,
         fontWeight: 'bold',
         padding: 10,
         // borderBottomColor: '#E3E3E3',
-        // borderBottomWidth: 1,
-        alignSelf: 'center'
+        // borderBottomWidth: 1
     },
     containerOne: {
         flex: 1,
@@ -435,9 +565,9 @@ const styles = StyleSheet.create({
     containerTwo: {
         position: 'absolute',
         top: 150,
-        left: 20,
-        right: 20,
-        bottom: 150,
+        left: 15,
+        right: 15,
+        bottom: 130,
         borderRadius: 10,
         borderColor: '#D8D8D8',
         borderWidth: 2,
@@ -463,5 +593,13 @@ const styles = StyleSheet.create({
         // marginBottom: 5,
         marginLeft: 15,
         marginRight: 15,
-    }
+    },
+    actionsContainer: {
+        flexDirection:'row',
+        borderRadius: 10,
+        backgroundColor: '#DCDCDC',
+        marginTop: 5,
+        justifyContent: 'space-between',
+        padding: 10
+    },
 });
